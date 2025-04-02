299690a9d30e20584aa2809656647ffa73f23dd1
Steve Carpio <carpios@tda-sgft.com>
2025-04-02 12:12:30 +0200

    ok

--- src/cfg/MAILING_variables.py
+++ src/cfg/MAILING_variables.py
@@ -3,23 +3,11 @@
 # Descripción: Variables necesarias para la ejecución del proceso.
 # Autor: SteveCarpio-2025
 # ----------------------------------------------------------------------------------------
-import os
+
 
 #-----------------------------------------------------------------------------------------
 #                          Funciones de APOYO
 # ----------------------------------------------------------------------------------------
-def valida_ruta_de_red():
-    ruta1="H:\\Proyectos\\Python\\MisCompilados\\PROY_CALENDARIZAR\\"  # Mapeado en mi PC
-    ruta2="H:\\MisCompilados\\PROY_CALENDARIZAR\\"                     # Mapeado en el server Python
-    # Verificar si alguna de las rutas existe
-    if os.path.isdir(ruta1):
-        ruta = ruta1
-    elif os.path.isdir(ruta2):
-        ruta = ruta2
-    else:
-        print(f"AVISO: Ninguna de las carpetas existe. {ruta1} - {ruta2} ")
-        ruta = "ERROR"
-    return ruta
 
 
 #-----------------------------------------------------------------------------------------
@@ -33,11 +21,10 @@ loc_RutaInforme=f'{loc_RutaRaiz}INFORMES\\'
 loc_RutaLog=f'{loc_RutaRaiz}LOG\\'
 
 # **** Red ****
-ruta=valida_ruta_de_red()
-red_RutaRaiz=ruta 
-red_RutaAccess=f"{red_RutaRaiz}BBDD\\"
-red_RutaConfig=f'{red_RutaRaiz}CONFIG\\'
-red_RutaInforme=f'{red_RutaRaiz}INFORMES\\'
+red_RutaRaiz=r'\\newton\comun$\Proyectos\Python\MisCompilados\PROY_CALENDARIZAR'
+red_RutaAccess=f"{red_RutaRaiz}\\BBDD\\"
+red_RutaConfig=f'{red_RutaRaiz}\\CONFIG\\'
+red_RutaInforme=f'{red_RutaRaiz}\\INFORMES\\'
 
 # ----------------------------------------------------------------------------------------
 #                          VARIABLES DE APOYO
