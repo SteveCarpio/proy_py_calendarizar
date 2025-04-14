b9b4a6bb04519c94d728a158510b306e35f1c775
Steve Carpio <carpios@tda-sgft.com>
2025-04-14 17:47:07 +0200

    zimbra cita tarea

--- src/ZIMBRA_Main.py
+++ src/ZIMBRA_Main.py
@@ -1,9 +1,19 @@
+# ----------------------------------------------------------------------------------------
+#                                  ZIMBRA SOAP: Tareas / Citas
+#
+# Programa que Añadirá información en las tareas y citas del Zimbra
+# Autor: SteveCarpio
+# Versión: V1 2025
+# ----------------------------------------------------------------------------------------
+
 import requests
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-
-def Crear_Token_SOAP(pUsuario, pContrasena):
+# ----------------------------------------------------------------------------------------
+#                               FUNCIONES DE APOYO
+# ----------------------------------------------------------------------------------------
+def Crear_Token_SOAP(pUrl, pUsuario, pContrasena):
 
     # XML de autenticación
     auth_xml = f"""<?xml version="1.0" encoding="UTF-8"?>
@@ -17,7 +27,7 @@ def Crear_Token_SOAP(pUsuario, pContrasena):
     </soap:Envelope>"""
 
     # Enviar petición
-    response = requests.post(url, data=auth_xml, headers={"Content-Type": "text/xml"}, verify=False)
+    response = requests.post(pUrl, data=auth_xml, headers={"Content-Type": "text/xml"}, verify=False)
 
     # Mostrar respuesta
     print("Código HTTP:", response.status_code)
@@ -39,46 +49,7 @@ def Crear_Token_SOAP(pUsuario, pContrasena):
 
     return auth_token
 
-def Crear_Tarea_SOAP(pAuthToken, pTitulo, pEstado, pPrioridad, pLocate, pDescribe, pContent, pSu, pOrganizador, pFIni, pFFin, pFRec):
-    crear_tarea_con_alarma = f"""<?xml version="1.0" encoding="UTF-8"?>
-    <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
-    <soap:Header>
-        <context xmlns="urn:zimbra">
-        <authToken>{pAuthToken}</authToken>
-        </context>
-    </soap:Header>
-    <soap:Body>
-        <CreateTaskRequest xmlns="urn:zimbraMail">
-        <m>
-            <inv method="REQUEST" type="task">
-            <comp name="{pTitulo}" percentComplete="0" status="{pEstado}" priority="{pPrioridad}">
-                <s d="{pFIni}"/>
-                <e d="{pFFin}"/>
-                <loc>{pLocate}</loc>
-                <alarm action="DISPLAY">
-                <trigger>
-                    <abs d="{pFRec}"/>
-                </trigger>
-                <desc>{pDescribe}</desc>
-                <attach uri=""/>
-                </alarm>
-            </comp>
-            </inv>
-            <su>{pSu}</su>
-            <mp ct="text/plain">
-            <content>{pContent}</content>
-            </mp>
-        </m>
-        </CreateTaskRequest>
-    </soap:Body>
-    </soap:Envelope>"""
-
-    # Enviar la solicitud
-    response2 = requests.post(url, data=crear_tarea_con_alarma, headers={"Content-Type": "text/xml"}, verify=False)
-    print("Respuesta al crear tarea:")
-    print(response2.text)
-
-def Crear_Cita_SOAP (pAuthToken, pTitulo, pEstado, pPrioridad, pLocate, pDescribe, pContent, pSu, pOrganizador, pREQ1, pREQ2, pOPT, pFIni, pFFin, pFRec):
+def Crear_Cita_SOAP (pUrl, pAuthToken, pTitulo, pEstado, pPrioridad, pLocate, pDescribe, pContent, pSu, pOrganizador, pREQ1, pREQ2, pOPT, pFIni, pFFin, pFRec):
     crear_cita_con_alarma = f"""<?xml version="1.0" encoding="UTF-8"?>
     <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
       <soap:Header>
@@ -116,42 +87,88 @@ def Crear_Cita_SOAP (pAuthToken, pTitulo, pEstado, pPrioridad, pLocate, pDescrib
     </soap:Envelope>"""
 
     # Enviar la solicitud
-    response = requests.post(url, data=crear_cita_con_alarma, headers={"Content-Type": "text/xml"}, verify=False)
+    response = requests.post(pUrl, data=crear_cita_con_alarma, headers={"Content-Type": "text/xml"}, verify=False)
     print("Respuesta al crear cita:")
     print(response.text)
 
+def Crear_Tarea_SOAP(pUrl, pAuthToken, pTitulo, pEstado, pPrioridad, pLocate, pDescribe, pContent, pSu, pFIni, pFFin, pFRec):
+    crear_tarea_con_alarma = f"""<?xml version="1.0" encoding="UTF-8"?>
+    <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+    <soap:Header>
+        <context xmlns="urn:zimbra">
+        <authToken>{pAuthToken}</authToken>
+        </context>
+    </soap:Header>
+    <soap:Body>
+        <CreateTaskRequest xmlns="urn:zimbraMail">
+        <m>
+            <inv method="REQUEST" type="task">
+            <comp name="{pTitulo}" percentComplete="0" status="{pEstado}" priority="{pPrioridad}">
+                <s d="{pFIni}"/>
+                <e d="{pFFin}"/>
+                <loc>{pLocate}</loc>
+                <alarm action="DISPLAY">
+                <trigger>
+                    <abs d="{pFRec}"/>
+                </trigger>
+                <desc>{pDescribe}</desc>
+                <attach uri=""/>
+                </alarm>
+            </comp>
+            </inv>
+            <su>{pSu}</su>
+            <mp ct="text/plain">
+            <content>{pContent}</content>
+            </mp>
+        </m>
+        </CreateTaskRequest>
+    </soap:Body>
+    </soap:Envelope>"""
 
-#########################################################################################
-url = "https://zimbra.tda-sgft.com/service/soap"
+    # Enviar la solicitud
+    response2 = requests.post(pUrl, data=crear_tarea_con_alarma, headers={"Content-Type": "text/xml"}, verify=False)
+    print("Respuesta al crear tarea:")
+    print(response2.text)
+
+
+
+# ----------------------------------------------------------------------------------------
+#                               INICIO DEL PROGRAMA
+# ----------------------------------------------------------------------------------------
+
+# ----------------- DATOS COMUNES TAREA/CITA
+vTitulo="Titulo de la TAREA / CITA"
+vSu=vTitulo                            # Sujeto - Titulo de la Alerta - Pop-up
+vDescribe="DESCRIBE: xxxxx"            # Descripción de la alerta     - Pop-up
+vLocate="Localización Clave Pizarra XXXXXXXX 3"     # Ubicación Tarea / Cita
+vContent="Contenido de la tarea XXXXXXXX \n xxxxxxxxxx"
+vFIni = "20250414T163000Z"             # 10:30 en España (UTC+2)
+vFFin = "20250414T164500Z"             # 10:45 fin
+vFRec = "20250414T161500Z"             # 10:15 Salta 15 minutos antes
+vUrl = "https://zimbra.tda-sgft.com/service/soap"
 
 print("----------------- CREAR LA TOKEN -----------------")
 vUsuario = "carpios@tda-sgft.com"
 #vUsuario = "talavanf@tda-sgft.com"
-vContrasena="xxxxx"
-#vAuthToken = Crear_Token_SOAP(vUsuario, vContrasena)
-# Steve
-x1="0_0c36221b747cab40e0c824aaa3335628595f7cc9_69643d33363a34383737623236352d313730322d343261372d386431652d3436313239396466363061313b6578703d31333a313734343739353038343830343b747970653d363a7a696d6272613b753d313a613b7469643d31303a313733333435333435343b76657273696f6e3d31343a31302e302e305f47415f343531383b"
-# Paco
-x2="0_21c891a3d7bda96dd766e5642b74b381c7492384_69643d33363a34383737623236352d313730322d343261372d386431652d3436313239396466363061313b6578703d31333a313734343739373234393930373b747970653d363a7a696d6272613b753d313a613b7469643d383a33333937373230353b76657273696f6e3d31343a31302e302e305f47415f343531383b"
-vAuthToken=x1
+#vUsuario = "publicacionesbolsasmx@tda-sgft.com"
+vContrasena="G3m4198005$$"
+#vContrasena="Mrpotato51.."
+#vContrasena="U9d8z?:8K,>2"
+vAuthToken = Crear_Token_SOAP(vUrl, vUsuario, vContrasena)
 
-print("----------------- CREAR LA TAREA -----------------")
-vTitulo="Creando un tarea de prueba 16"
-vEstado="INPR"                         # NEED: No se ha iniciado | INPR: En progreso | COMP: Completada | WAITING: En espera | DEFERRED: Pospuesta
-vPrioridad="1"                         # 1   : Alta
-vLocate="Clave Pizarra XXXXXXXX 3"
-vDescribe="DESCRIBE: xxxxx"            # ¿.................?
-vContent="Contenido de la tarea XXXXXXXX \n xxxxxxxxxx"
-vSu="SU: xxxxxxxxxx"                   # ¿.................?
-vOrganizador="carpios@tda-sgft.com"
-pREQ1="talavanf@tda-sgft.com"
-pREQ2="blancod@tda-sgft.com"
-pOPT="carpios@tda-sgft.com"
-vFIni = "20250414T163000Z"  # 10:30 en España (UTC+2)
-vFFin = "20250414T164500Z"
-vFRec = "20250414T161500Z"  # Salta 15 minutos antes
+print("----------------- CREAR UNA CITA -----------------")
+vEstado1="CONF"                         # CONF: confirmado (por defecto) | TENT: Tentativo/Provisional | CANC: Cancelado
+vPrioridad1="5"                         # 1: Alta, 5: Normal (recomendado), 9: Baja
+vOrganizador="carpios@tda-sgft.com"     # Email del organizador
+pREQ1="talavanf@tda-sgft.com"           # Email de las personas requeridas 1
+pREQ2="blancod@tda-sgft.com"            # Email de las personas requeridas n
+pOPT="carpios@tda-sgft.com"             # Email de las personas opcionales
+Crear_Cita_SOAP(vUrl, vAuthToken, vTitulo, vEstado1, vPrioridad1, vLocate, vDescribe, vContent, vSu, vOrganizador, pREQ1, pREQ2, pOPT, vFIni, vFFin, vFRec)
 
-Crear_Tarea_SOAP(vAuthToken, vTitulo, vEstado, vPrioridad, vLocate, vDescribe, vContent, vSu, vOrganizador, vFIni, vFFin, vFRec)
+print("----------------- CREAR UNA TAREA ----------------")
+vEstado2="INPR"                         # NEED:No se ha iniciado |INPR:En progreso |COMP:Completada |WAITING:En espera |DEFERRED:Pospuesta |CANCELLED:Cancelado 
+vPrioridad2="1"                         # 1: Alta, 5: Normal, 9: Baja
+Crear_Tarea_SOAP(vUrl, vAuthToken, vTitulo, vEstado2, vPrioridad2, vLocate, vDescribe, vContent, vSu, vFIni, vFFin, vFRec)
 
-#Crear_Cita_SOAP(vAuthToken, vTitulo, vEstado, vPrioridad, vLocate, vDescribe, vContent, vSu, vOrganizador, pREQ1, pREQ2, pOPT, vFIni, vFFin, vFRec)
+
                 
